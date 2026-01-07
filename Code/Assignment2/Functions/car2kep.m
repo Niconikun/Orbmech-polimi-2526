6161402db26fa054bcaecc458e2e24d9bf9d9433
Niconikun <nicolassepulvedaestrada@gmail.com>
2026-01-07 16:51:55 +0100

    Update car2kep.m

--- Code/Assignment2/Functions/car2kep.m
+++ Code/Assignment2/Functions/car2kep.m
@@ -1,6 +1,8 @@
 function [kep] = car2kep(r, v, mu)
 % car2kep Conversion from state vector to keplerian element vector
 % 
+% Function that compute keplerial element vector from the state vector
+% 
 % PROTOTYPE
 %  [kep] = car2kep(r, v, mu)
 % 
@@ -13,54 +15,35 @@ function [kep] = car2kep(r, v, mu)
 %   kep [6x1]        Keplerian element vector
 % 
 % CONTRIBUTORS:
-%   Muscas Alice, Masiero Federico
+%   Muscas Alice, Masiero Federico, Karthikeyan Prthik Nandhan, Nicolás Sepúlveda
 % 
 % VERSIONS
 %   2025-10-22: First version
 %   2025-10-14: Second version
 %   2025-11-12: Third version
-% 
-% -------------------------------------------------------------------------
+%   2026-01-03: Fourth version
 
-% 1. Check and ensure vectors are column vectors
-if length(r) ~= 3
-    error("r must be a vector of length 3")
-end
-if length(v) ~= 3
-    error("v must be a vector of length 3")
-end
 
-% Convert to column vectors if necessary
-r = r(:);
-v = v(:);
-
-% 2. Definition of z_vec, x_vec, toll
+% 0. Definition of z_vec, x_vec, toll
 z_vec = [0; 0; 1];
 x_vec = [1; 0; 0];
-toll = 1e-10;  % Reduced tolerance
+toll = 1e-3;
 
-% 3. Normalization of r and v
+% 1. Normalization of r and v
 r_norm = norm(r);
 v_norm = norm(v);
 
-% 4. Compute radial velocity v_r
+% 2. Compute radial velocity v_r
 v_r = dot(r, v) / r_norm;
 
-% 5. Angular momentum h
+% 3. Angular momentum h
 h = cross(r, v);
 h_norm = norm(h);
 
-% Handle special case of zero angular momentum (degenerate orbit)
-if h_norm < toll
-    % Use a default direction for h
-    h = [0; 0; 1];
-    h_norm = 1;
-end
-
-% 6. Inclination i
+% 4. Inclination i
 i = acos(h(3) / h_norm);
 
-% 7. Line of node
+% 5. Line of node
 N = cross(z_vec, h);
 N_norm = norm(N);
 if N_norm < toll
@@ -68,88 +51,70 @@ if N_norm < toll
     N_norm = 1;
 end
 
-% Ensure N is a column vector
-N = N(:);
-
-% 8. Eccentricity vector
-e_vec = (1/mu) * ((v_norm^2 - mu/r_norm) * r - (r_norm * v_r) * v);
+% 6. Eccentricity
+e_vec = (1/mu) * ( (v_norm^2 - mu / r_norm) .* r - (r_norm*v_r) .* v );
 e_norm = norm(e_vec);
 e = e_norm;
-
-if e_norm < toll
-    e_vec = x_vec;  % Use default direction for circular orbits
+if e_norm  < toll
+    e_vec = x_vec;
     e_norm = 1;
     e = 0;
-else
-    e_vec = e_vec(:);  % Ensure column vector
 end
 
-% 9. RAAN (Right Ascension of Ascending Node)
+% 7. RAAN
 if isequal(N, x_vec)
     Omega = 0;
 else
-    cos_Omega = N(1) / N_norm;
-    % Clamp to [-1, 1] to avoid numerical issues
-    cos_Omega = max(-1, min(1, cos_Omega));
     if N(2) >= 0
-        Omega = acos(cos_Omega);
+        Omega = real(acos(N(1) / N_norm));
     else
-        Omega = 2*pi - acos(cos_Omega);
+        Omega = real(2*pi - acos(N(1) / N_norm));
     end
 end
 
-% 10. Argument of the pericentre
-if e < toll
-    omega = 0;
-else
-    cos_omega = dot(N, e_vec) / (e_norm * N_norm);
-    % Clamp to [-1, 1]
-    cos_omega = max(-1, min(1, cos_omega));
-    if e_vec(3) >= 0
-        omega = acos(cos_omega);
+% 8. Argument of the pericentre
+if (i ~= 0) && (i ~= pi)
+    if e == 0
+        omega = 0;
     else
-        omega = 2*pi - acos(cos_omega);
-    end
-end
-
-% 11. True anomaly
-if e < toll
-    % For circular orbits, use the angle from the node line
-    if N_norm > toll
-        cos_theta = dot(N, r) / (r_norm * N_norm);
-        cos_theta = max(-1, min(1, cos_theta));
-        if r(3) >= 0
-            theta = acos(cos_theta);
+        if e_vec(3) >= 0
+            omega = real(acos( dot(N, e_vec) / (e_norm*N_norm) ));
         else
-            theta = 2*pi - acos(cos_theta);
+            omega = real(2*pi - acos( dot(N, e_vec) / (e_norm*N_norm) ));
         end
-    else
-        theta = 0;
     end
-else
-    cos_theta = dot(e_vec, r) / (e_norm * r_norm);
-    cos_theta = max(-1, min(1, cos_theta));
-    if v_r >= 0
-        theta = acos(cos_theta);
+
+elseif i == 0
+    if e_vec(2) >= 0
+          omega = real(acos( dot(N, e_vec) / (e_norm*N_norm) ));
     else
-        theta = 2*pi - acos(cos_theta);
+          omega = real(2*pi - acos( dot(N, e_vec) / (e_norm*N_norm) ));
+    end
+
+elseif i == pi
+    if e_vec(2) <= 0
+          omega = real(acos( dot(N, e_vec) / (e_norm*N_norm) ));
+    else
+          omega = real(2*pi - acos( dot(N, e_vec) / (e_norm*N_norm) ));
     end
 end
 
-% 12. Semi major axis
+% 8. True anomaly
+if v_r >= -1e-14 
+    theta = real(acos( dot(e_vec, r) ./ (e_norm*r_norm) ));
+else
+    theta = real(2*pi - acos( dot(e_vec, r) ./ (e_norm*r_norm) ));
+end
+if e == 0
+    omega = theta;
+    theta = 0;
+end
+
+% 9. Semi major axis
 energy = (v_norm^2 / 2) - (mu / r_norm);
-if abs(energy) < toll
-    a = Inf;  % Parabolic orbit
-else
-    a = -mu / (2 * energy);
-end
+a = - mu / (2 * energy);
 
-% 13. Keplerian element vector
+% 10. kep
 kep = [a; e; i; Omega; omega; theta];
-
-% Normalize angles to [0, 2π)
-kep(6) = mod(kep(6), 2*pi);  % True anomaly
-kep(5) = mod(kep(5), 2*pi);  % Argument of periapsis
-kep(4) = mod(kep(4), 2*pi);  % RAAN
 
 end
